9f23c49bc5f8ea5d55dae9c222f30798621fd90f
Mullen <mullensh@amazon.com>
2019-12-10 12:20:57 -0800

    added jeopardy

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Welcome from './components/welcome/Welcome';
 import Clock from './components/clock/Clock';
 import Contact from './components/contact/Contact';
 import Page404 from './components/page404/Page404';
-//Import the Navigation component
+import Jeopardy from './components/jeopardy/Jeopardy';
 import Navigation from './components/navigation/Navigation';
 //Import the Route component
 import { Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
       <Route path="/clock" component={Clock} /> 
       <Route path="/contact" component={Contact} />
       <Route component={Page404} />
+      <Route path="/jeopardy" component={Jeopardy}/>
     </Switch>
       <Navigation />
     </div>
